09aad29f2221c96fc86d5a2259cff2500d3f978f
Mathys Henneron <henneron.mathyss@gmail.com>
2024-06-23 16:05:27 +0200

    Correction redirection page error

--- src/pages/error.jsx
+++ src/pages/error.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Error() {
-  <main>
-    <div className="error">
-      <h1>Erreur 404 - Page non trouvée</h1>
-      <p>La page que vous recherchez n'existe pas.</p>
-      <Link to="/"> Revenir à la page d'accueil</Link>
-    </div>
-  </main>;
+  return (
+    <main>
+      <div className="error">
+        <h1>Erreur 404 - Page non trouvée</h1>
+        <p>La page que vous recherchez n'existe pas.</p>
+        <Link to="/"> Revenir à la page d'accueil</Link>
+      </div>
+    </main>
+  );
 }
 
 export default Error;
